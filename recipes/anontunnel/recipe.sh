16f4e3aee106bbb112fadfcfb2342b67749fdcb5
lfdversluis <laurens-versluis@hotmail.com>
2014-05-08 16:21:43 +0200

    added netifaces and m2crypto. Fixed #27

--- recipes/anontunnel/recipe.sh
+++ recipes/anontunnel/recipe.sh
@@ -13,13 +13,17 @@ DEPS_anontunnel=(kivy)
 URL_anontunnel=http://ios-dev.no-ip.org/anontunnel-$VERSION_anontunnel.tar.gz
 
 # md5 of the package
-MD5_anontunnel=57baeb8e1e7a1785398c6d7e9e26b639
+MD5_anontunnel=57892630411719ef0dd3cee48d025eec
 
 # default build path
 BUILD_anontunnel=$BUILD_PATH/anontunnel/$(get_directory $URL_anontunnel)
 
 # default recipe path
 RECIPE_anontunnel=$RECIPES_PATH/anontunnel
+
+# set the correct source and destination for the curves.ec file (this is not automatically done so we have to do it)
+LOGGER_source=$BUILD_anontunnel/anontunnel/logger.conf
+LOGGER_dest=$BUILD_PATH/python-install/lib/python2.7/site-packages/anontunnel/logger.conf
 
 # function called for preparing source code if needed
 # (you can apply patch etc here.)
@@ -37,5 +41,7 @@ function build_anontunnel() {
 
 # function called after all the compile have been done
 function postbuild_anontunnel() {
-	true
+	# logger.conf probably gets ignored by setup.py install,
+	# copy it manually to site-packages of the built python
+	try cp $LOGGER_source $LOGGER_dest
 }
