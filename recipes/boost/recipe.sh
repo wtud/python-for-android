09f2af0ecfec00d126be1f85eeaf826116c0cb4a
Rolf Jagerman <rjagerman@gmail.com>
2014-05-12 10:39:17 +0200

    Update recipe for boost to not compile but just provide patched source for libtorrent

--- recipes/boost/recipe.sh
+++ recipes/boost/recipe.sh
@@ -4,7 +4,7 @@
 #VERSION_boost=${VERSION_boost:-1.55.0}
 
 # dependencies of this recipe
-DEPS_boost=()
+DEPS_boost=(python)
 
 # url of the package
 URL_boost=http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz
@@ -23,29 +23,34 @@ RECIPE_boost=$RECIPES_PATH/boost
 function prebuild_boost() {
 	cd $BUILD_boost
 
-	# Create build tools for boost
-	./bootstrap.sh
-	
-	# Write android config to boost user-config instructions
-	NDKBIN=${ANDROIDNDK}/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86
-	USRCONFIG=${BUILD_boost}/tools/build/v2/user-config.jam
+	# Copy android config to boost's user-config.jam
+	RECIPECONFIG=${RECIPE_boost}/user-config.jam
+	BOOSTCONFIG=${BUILD_boost}/tools/build/v2/user-config.jam
 
-	echo "# Android Configuration " > $USRCONFIG
-	echo "local AndroidToolchainRoot = ${NDKBIN}" >> $USRCONFIG
-	echo "using gcc" >> $USRCONFIG
-	echo ": androidR9" >> $USRCONFIG
-	echo ": \$(AndroidToolchainRoot)/bin/arm-linux-androideabi-g++" >> $USRCONFIG
-	echo ";" >> $USRCONFIG
+	# Insert android path, arm version and api version into user-config.jam
+	try echo "local AndroidNDK = ${ANDROIDNDK} ; " > ${BOOSTCONFIG}
+	try echo "local ARMToolchainVersion = 4.8 ; " >> ${BOOSTCONFIG}
+	try echo "local AndroidAPIVersion = 14 ; " >> ${BOOSTCONFIG}
+	try echo "local PythonInstall = ${BUILD_PATH}/python-install ; " >> ${BOOSTCONFIG}
+	try cat ${RECIPECONFIG} >> ${BOOSTCONFIG}
+
+	# Create build tools for boost
+	./bootstrap.sh 
+
+	# Apply source code patches for Android
+	patch -p1 < ${RECIPE_boost}/patches/boost_1_55_0/boost-1_55_0.patch
 }
 
 # function called to build the source code
 function build_boost() {
 	cd $BUILD_boost
 	
+	#!!! do not build boost, we just use it as source for libtorrent!!!
+
 	# Build boost with arm architecture
-	push_arm
-	./b2 toolset=gcc-androidR9
-	pop_arm
+	#push_arm
+	#./b2 toolset=gcc-androidR9 link=static 
+	#pop_arm
 }
 
 # function called after all the compile have been done
