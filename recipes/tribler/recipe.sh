33eb2f3d46654d63e4e1763d78e1028f2e8bf543
lfdversluis <laurens-versluis@hotmail.com>
2014-06-04 13:25:28 +0200

    removed md5 check temporarily

--- recipes/tribler/recipe.sh
+++ recipes/tribler/recipe.sh
@@ -14,7 +14,7 @@ DEPS_tribler=(kivy openssl)
 URL_tribler=https://github.com/devos50/Tribler/raw/tribler-sessiontest/Tribler-$VERSION_tribler.tar.gz
  
 # md5 of the package
-MD5_tribler=c7f4542ee150ddd724efacb7c8ff1200
+MD5_tribler=
  
 # default build path
 BUILD_tribler=$BUILD_PATH/tribler/$(get_directory $URL_tribler)
